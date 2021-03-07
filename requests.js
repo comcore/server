e330d4448e48fc07f6f01c29e0cf25af19ced416
Scott Taylor <taylo840@purdue.edu>
2021-03-06 20:29:40 -0500

    Implemented server with stubs for database access

---
+++ requests.js
@@ -0,0 +1,184 @@
+/*
+ * Represents an unexpected error in handling a request (e.g. the request is invalid in a way that
+ * should have been checked by the client beforehand). The server will forward the error message to
+ * the client.
+ */
+class RequestError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "RequestError";
+  }
+}
+
+/*
+ * Generate a code and send it to the requested email address.
+ */
+async function sendCode(email, forReset) {
+  throw new RequestError('unimplemented: sendCode');
+}
+
+/*
+ * Look up an account by email. If the account doesn't exist, return null. Otherwise return:
+ *
+ * {
+ *   id:   the ID of the user,
+ *   name: the name of the user,
+ *   pass: the stored hashed password of the user,
+ * }
+ */
+async function lookupAccount(email) {
+  throw new RequestError('unimplemented: lookupAccount');
+}
+
+/*
+ * Create a new account with an associated name, email address, and hashed password. If an account
+ * with the given email address already exists, return null. Otherwise return the user ID of the
+ * newly created user.
+ */
+async function createAccount(name, email, pass) {
+  throw new RequestError('unimplemented: createAccount');
+}
+
+/*
+ * Reset the password of an account specified by a user ID to have the provided hashed password.
+ */
+async function resetPassword(user, pass) {
+  throw new RequestError('unimplemented: resetPassword');
+}
+
+/*
+ * Create a new group owned by the specified user ID with the given name. Return the group ID of
+ * the newly created group.
+ */
+async function createGroup(user, name) {
+  throw new RequestError('unimplemented: createGroup');
+}
+
+/*
+ * Get a list of the groups which a user is part of. Each entry in the array should look like:
+ *
+ * {
+ *   id:    the ID of the group,
+ *   name:  the name of the group,
+ *   role:  'owner' | 'moderator' | 'user',
+ *   muted: false | true,
+ * }
+ */
+async function getGroups(user) {
+  throw new RequestError('unimplemented: getGroups');
+}
+
+/*
+ * Create a new chat in the given group ID with the given name. Make sure that the user ID is part
+ * of the group before creating the chat, and throw a RequestError if they are not authorized.
+ * Return the chat ID of the new chat.
+ */
+async function createChat(user, group, name) {
+  throw new RequestError('unimplemented: createChat');
+}
+
+/*
+ * Get a list of the users in a group. Make sure that the user ID is part of the group before
+ * creating the list, and throw a RequestError if they are not authorized. Each entry in the array
+ * should look like:
+ *
+ * {
+ *   id:    the ID of the user,
+ *   name:  the name of the user,
+ *   role:  'owner' | 'moderator' | 'user',
+ *   muted: false | true,
+ * }
+ */
+async function getUsers(user, group) {
+  throw new RequestError('unimplemented: getUsers');
+}
+
+/*
+ * Get a list of the chats in a group. Make sure that the user ID is part of the group before
+ * creating the list, and throw a RequestError if they are not authorized. Each entry in the array
+ * should look like:
+ *
+ * {
+ *   id:   the ID of the chat,
+ *   name: the name of the chat,
+ * }
+ */
+async function getChats(user, group) {
+  throw new RequestError('unimplemented: getChats');
+}
+
+/*
+ * Set the role of another user in a group. Make sure that both users are part of the group and that
+ * 'user' has a more powerful role than 'targetUser' before setting the role. If the 'owner' sets
+ * another user's role to 'owner', make the original user a 'moderator' in order to transfer
+ * ownership (since a group can only have one owner). A user cannot change their own role, so also
+ * check that the two users are different. Throw a RequestError if the request is invalid.
+ */
+async function setRole(user, group, targetUser, role) {
+  throw new RequestError('unimplemented: setRole');
+}
+
+/*
+ * Set the muted status of another user in a group. Make sure that both users are part of the group
+ * and that 'user' has a more powerful role than 'targetUser' before setting the muted status. A
+ * user cannot mute/unmute themselves, so also check that the two users are different. Throw a
+ * RequestError if the request is invalid.
+ */
+async function setMuted(user, group, targetUser, muted) {
+  throw new RequestError('unimplemented: setMuted');
+}
+
+/*
+ * Send a message in a chat in a group. Make sure that the user ID is part of the group, that
+ * the chat is part of the group, and that the user isn't muted before sending the message,
+ * and throw a RequestError if the request is invalid.
+ *
+ * Messages should be assigned a sequential ID starting with 1 in each chat, such that the first
+ * message in a chat has ID 1, then the second has 2, then 3, 4, and so on. The database should
+ * store enough information to satisfy the requests in getMessages(). A UNIX timestamp is provided
+ * in the format of number of milliseconds since January 1, 1970. The message ID and timestamp are
+ * numbers, not strings.
+ *
+ * Return the message ID of the newly added message.
+ */
+async function sendMessage(user, group, chat, timestamp, contents) {
+  throw new RequestError('unimplemented: sendMessage');
+}
+
+/*
+ * Get a set of messages in the chat. Make sure that the user ID is part of the group, and that
+ * the chat is part of the group before creating the list, and throw a RequestError if the request
+ * is invalid.
+ *
+ * The message IDs and timestamp are numbers, not strings.
+ *
+ * All messages with an ID greater than 'after' and less than 'before' should be returned, with
+ * each entry in the array looking like:
+ *
+ * {
+ *   id:        the sequential ID of the message,
+ *   sender:    the user ID of the sender,
+ *   timestamp: the UNIX timestamp representing when the message was sent,
+ *   contents:  the contents of the message as a string,
+ * }
+ */
+async function getMessages(user, group, chat, after, before) {
+  throw new RequestError('unimplemented: getMessages');
+}
+
+module.exports = {
+  RequestError,
+  sendCode,
+  lookupAccount,
+  createAccount,
+  resetPassword,
+  createGroup,
+  getGroups,
+  createChat,
+  getUsers,
+  getChats,
+  setRole,
+  setMuted,
+  sendMessage,
+  getMessages,
+};
--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const fs = require('fs');
 
 const Dequeue = require('dequeue');
 
+const requests = require('./requests');
+const { RequestError } = requests;
+
+const CODE_RESET_INTERVAL = 60 * 60 * 1000;
+const CODE_MAX_FAILS = 3;
+
 class Connection {
   constructor(server, socket) {
     this.server = server;
@@ -10,7 +16,10 @@ class Connection {
     this.isCancelled = false;
     this.isBusy = false;
     this.lineBuffer = '';
-    this.waitingCommands = new Dequeue();
+    this.waitingRequests = new Dequeue();
+    this.userInfo = null;
+    this.pendingCode = false;
+    this.canReset = false;
 
     socket.on('data', data => {
       this.lineBuffer += data;
@@ -19,11 +28,11 @@ class Connection {
 
       lines.forEach(line => {
         if (line) {
-          this.waitingCommands.push(line);
+          this.waitingRequests.push(line);
         }
       });
 
-      this.checkCommand();
+      this.checkRequest();
     });
   }
 
@@ -31,34 +40,240 @@ class Connection {
     this.isCancelled = true;
   }
 
-  checkCommand() {
-    if (this.isBusy || this.waitingCommands.length < 1) {
+  ensureNotLoggedIn() {
+    if (this.userInfo) {
+      this.logout();
+    }
+  }
+
+  async login(id, email, codeCanReset) {
+    this.ensureNotLoggedIn();
+
+    this.userInfo = { id, email };
+
+    if (codeCanReset === null) {
+      this.pendingCode = false;
+    } else {
+      await this.server.generateCode(id, email, codeCanReset);
+      this.pendingCode = true;
+    }
+
+    this.server.loginConnection(id, this);
+  }
+
+  logout() {
+    if (this.userInfo) {
+      this.server.logoutConnection(this.userInfo.id, this);
+      this.userInfo = null;
+    }
+  }
+
+  userID() {
+    return this.userInfo && !this.pendingCode ? this.userInfo.id : null;
+  }
+
+  checkRequest() {
+    if (this.isCancelled || this.isBusy || this.waitingRequests.length < 1) {
       return;
     }
 
-    const command = this.waitingCommands.shift();
+    const request = this.waitingRequests.shift();
     this.isBusy = true;
-    this.startCommand(command);
+    this.runRequest(request)
+      .then(response =>
+        this.finishRequest('REPLY', response))
+      .catch(err => {
+        let message;
+        if (err instanceof RequestError) {
+          message = err.message;
+        } else {
+          console.error(err);
+          message = 'internal server error';
+        }
+        this.finishRequest('ERROR', { message });
+      });
   }
 
-  startCommand(command) {
+  notify(kind, data) {
     if (this.isCancelled) {
       return;
     }
 
-    console.log(command);
-    this.finishCommand(command);
+    this.socket.write(JSON.stringify({ kind, data }) + '\n');
   }
 
-  finishCommand(response) {
-    if (this.isCancelled) {
-      return;
-    }
-
-    response += '\n'
-    this.socket.write(response);
+  finishRequest(kind, data) {
+    this.notify(kind, data);
     this.isBusy = false;
-    this.checkCommand();
+    this.checkRequest();
+  }
+
+  forceLogout() {
+    this.logout();
+    this.notify("logout", {});
+  }
+
+  receiveMessage(message) {
+    this.notify("message", message);
+  }
+
+  async runRequest(request) {
+    const { kind, data } = JSON.parse(request);
+    switch (kind) {
+      case 'logout': {
+        this.logout();
+        return {};
+      }
+      case 'login': {
+        this.ensureNotLoggedIn();
+
+        const { email, pass } = data;
+        const account = await requests.lookupAccount(email);
+        if (!account) {
+          return { status: 'DOES_NOT_EXIST' };
+        }
+
+        if (pass !== account.pass) {
+          return { status: 'INVALID_PASSWORD' };
+        }
+
+        await this.login(account.id, email, null);
+        return { status: 'SUCCESS' };
+      }
+      case 'createAccount': {
+        this.ensureNotLoggedIn();
+
+        const { name, email, pass } = data;
+        const id = await requests.createAccount(name, email, pass);
+        if (!id) {
+          return { created: false };
+        }
+
+        await this.login(id, email, false);
+        return { created: true };
+      }
+      case 'requestReset': {
+        this.ensureNotLoggedIn();
+
+        const { email } = data;
+        const account = await requests.lookupAccount(email);
+        if (!account) {
+          return { sent: false };
+        }
+
+        await this.login(account.id, email, true);
+        return { sent: true };
+      }
+      case 'enterCode': {
+        if (!this.userInfo) {
+          throw new RequestError('enterCode called without logging in');
+        }
+
+        const { code } = data;
+        const codeCanReset = this.server.verifyCode(this.userInfo.id, code);
+        if (codeCanReset === null) {
+          return { correct: false };
+        }
+
+        this.canReset = codeCanReset;
+        this.pendingCode = false;
+        return { correct: true };
+      }
+      default:
+        const user = this.userID();
+        if (!user) {
+          throw new RequestError('unauthorized');
+        }
+
+        return await this.runAuthorizedRequest(user, kind, data);
+    }
+  }
+
+  async runAuthorizedRequest(user, kind, data) {
+    switch (kind) {
+      case 'finishReset': {
+        const { pass } = data;
+        if (!this.canReset) {
+          return { reset: false };
+        }
+
+        await requests.resetPassword(user, pass);
+        this.server.forceLogout(user, this);
+        return { reset: true };
+      }
+      case 'createGroup': {
+        const { name } = data;
+        const id = await requests.createGroup(user, name);
+        return { id };
+      }
+      case 'getGroups': {
+        const groups = await requests.getGroups(user);
+        return { groups };
+      }
+      case 'createChat': {
+        const { group, name } = data;
+        const id = await requests.createChat(user, group, name);
+        return { id };
+      }
+      case 'getUsers': {
+        const { group } = data;
+        const users = await requests.getUsers(user, group);
+        return { users };
+      }
+      case 'getChats': {
+        const { group } = data;
+        const chats = await requests.getChats(user, group);
+        return { chats };
+      }
+      case 'setRole': {
+        const { group, target, role } = data;
+        await requests.setRole(user, group, target, role);
+        return {};
+      }
+      case 'setMuted': {
+        const { group, target, muted } = data;
+        await requests.setMuted(user, group, target, muted);
+        return {};
+      }
+      case 'sendMessage': {
+        const { group, chat, contents } = data;
+        const timestamp = Date.now();
+        const chatUsers = await requests.getUsers(user, group);
+        const id = await requests.sendMessage(user, group, chat, timestamp, contents);
+        const message = {
+          group,
+          chat,
+          id,
+          sender: user,
+          timestamp,
+          contents,
+        };
+        chatUsers.forEach(chatUser => {
+          if (chatUser.id !== user) {
+            this.server.forwardMessage(chatUser.id, message);
+          }
+        });
+        return {};
+      }
+      case 'getMessages': {
+        let { group, chat, after, before } = data;
+
+        if (after < 1) {
+          // All messages are higher than 1
+          after = 0;
+        }
+
+        if (before < 1) {
+          // All messages are lower than 2^52
+          before = 0x20000000000000;
+        }
+
+        const messages = await requests.getMessages(user, group, chat, after, before);
+        return { messages };
+      }
+      default:
+        throw new RequestError('unknown message kind: ' + kind);
+    }
   }
 }
 
@@ -70,6 +285,8 @@ class Server {
     };
 
     this.connections = new Set();
+    this.loggedIn = new Map();
+    this.pendingCodes = new Map();
 
     this.server = tls.createServer(options, socket => {
       socket.setEncoding('utf8');
@@ -83,6 +300,85 @@ class Server {
     });
 
     this.server.listen(443);
+  }
+
+  loginConnection(id, connection) {
+    const connections = this.loggedIn.get(id);
+    if (connections) {
+      connections.add(connection);
+    } else {
+      this.loggedIn.set(id, new Set([connection]));
+    }
+  }
+
+  logoutConnection(id, connection) {
+    const connections = this.loggedIn.get(id);
+    if (connections) {
+      connections.delete(connection);
+      if (connections.size === 0) {
+        this.loggedIn.delete(id);
+      }
+    }
+  }
+
+  async generateCode(id, email, forReset) {
+    const codeEntry = this.pendingCodes.get(id);
+    const time = Date.now();
+    if (codeEntry && codeEntry.forReset === forReset && time < codeEntry.expireTime) {
+      return;
+    }
+
+    const code = await requests.sendCode(email, forReset);
+    const expireTime = time + CODE_RESET_INTERVAL;
+    this.pendingCodes.set(id, { code, forReset, expireTime, fails: 0 });
+
+    return;
+  }
+
+  verifyCode(id, code) {
+    const codeEntry = this.pendingCodes.get(id);
+    if (!codeEntry) {
+      return null;
+    }
+
+    if (codeEntry.code !== code) {
+      codeEntry.fails++;
+      if (codeEntry.fails >= CODE_MAX_FAILS) {
+        this.pendingCodes.delete(id);
+      }
+      return null;
+    }
+
+    this.pendingCodes.delete(id);
+    if (Date.now() < codeEntry.expireTime) {
+      return codeEntry.forReset;
+    }
+
+    return null;
+  }
+
+  forceLogout(id, exceptFor) {
+    const connections = this.loggedIn.get(id);
+    if (!connections) {
+      return;
+    }
+
+    connections.forEach(connection => {
+      if (connection !== exceptFor) {
+        connection.forceLogout();
+      }
+    });
+  }
+
+  forwardMessage(id, message) {
+    const connections = this.loggedIn.get(id);
+    if (!connections) {
+      return;
+    }
+
+    connections.forEach(connection => {
+      connection.receiveMessage(message);
+    });
   }
 }
 
